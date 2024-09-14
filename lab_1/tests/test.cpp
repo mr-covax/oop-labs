64cc2fba521e34ed9c740f9de964be33be8f37d0
Timothy (mr-covax) <180854280+mr-covax@users.noreply.github.com>
2024-09-14 12:10:27 +0300

    [lab-1] Update test function signatures

--- lab_1/tests/test.cpp
+++ lab_1/tests/test.cpp
@@ -1,25 +1,25 @@
 #include <gtest/gtest.h>
 #include "../include/divider.hpp"
 
-TEST(base_case, basic_test_set) {
+TEST(bar_div_test, base_case) {
     ASSERT_TRUE(get_min_divs(5, 5) == 8);       // 4 h-cuts, 4 v-cuts
     ASSERT_TRUE(get_min_divs(100, 100) == 198); // 99 h-cuts, 99 v-cuts
 }
 
-TEST(no_bar, basic_test_set) {
+TEST(bar_div_test, no_bar) {
     ASSERT_TRUE(get_min_divs(0, 0) == 0);       // No bar at all :(
 }
 
-TEST(single_square_bar, basic_test_set) {
+TEST(bar_div_test, single_square) {
     ASSERT_TRUE(get_min_divs(1, 1) == 0);       // A single square
 }
 
-TEST(side_equals_one, basic_test_set) {
+TEST(bar_div_test, side_equals_one) {
     ASSERT_TRUE(get_min_divs(6, 1) == 5);       // Only horizontal cuts
     ASSERT_TRUE(get_min_divs(1, 6) == 5);       // Only vertical cuts
 }
 
-TEST(negative_sides, basic_test_set) {
+TEST(bar_div_test, negative_sides) {
     ASSERT_TRUE(get_min_divs(-1, 2) == 0);
     ASSERT_TRUE(get_min_divs(2, -1) == 0);
     ASSERT_TRUE(get_min_divs(-2, -1) == 0);
